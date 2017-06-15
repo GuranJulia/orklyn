3d65e119351320a2c21f9c444283b3ebdaf65dca
julia <guran-julia@hotmail.com>
2017-06-15 20:46:05 +0300

    first

---
+++ index.php
@@ -0,0 +1,169 @@
+<HTML>
+
+<HEAD>
+<META name="AUTHOR" content="OneHalf - mailto:onehalf2000@yandex.ru">
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+
+<META NAME="DESCRIPTION" CONTENT="Поезія як різновид Руху Опору.">
+<META NAME="KEYWORDS" CONTENT="українська сучасна поезія, Віктор Могильний, Сайт Віктора Могильного, Орклин, Віть Вітько, Надкушене яблуко, рух опору, химерики, лімерики, дисидент, вірші, СБУ, КДБ, КГБ, Дажбог, Хорс, Ратич, Мазепа, Київ, Москва, Тель-Авів, ukrainian modern lyrick">
+
+<TITLE>САЙТ ВІКТОРА МОГИЛЬНОГО</TITLE>
+</HEAD>
+
+<BODY bgcolor="black" text="grea" link="#3399FF" vlink="magenta" alink="black">
+
+<BR><CENTER>
+<HR width="500">
+<FONT face="Garamond, Courier New, Tahoma" color="red" size="+4"><B>САЙТ ВІКТОРА МОГИЛЬНОГО</B></font>
+<HR width="400" size="4">
+</CENTER><BR>
+
+<SCRIPT LANGUAGE="JavaScript">
+<!--
+if (document.images)
+{
+var hoff = new Image();
+hoff.src = "img/bdahim.gif";
+var honn = new Image();
+honn.src = "img/bahim.gif";
+var doff = new Image();
+doff.src = "img/bdadit.gif";
+var donn = new Image();
+donn.src = "img/badit.gif";
+var ooff = new Image();
+ooff.src = "img/bdaold.gif";
+var oonn = new Image();
+oonn.src = "img/baold.gif";
+var boff = new Image();
+boff.src = "img/bdabook.gif";
+var bonn = new Image();
+bonn.src = "img/babook.gif";
+var aoff = new Image();
+aoff.src = "img/bdaab.gif";
+var aonn = new Image();
+aonn.src = "img/baab.gif";
+
+}
+function swap(imgName)
+{
+ if (document.images) document[imgName].src = eval(imgName + 'onn.src');
+}
+function restore(imgName)
+{
+if (document.images) document[imgName].src = eval(imgName + 'off.src');
+}
+// -->
+</SCRIPT>
+
+<CENTER>
+<TABLE border="0" width="90%">
+
+<TR>
+<TD rowspan="3" width="100" align="left">
+
+</TD>
+<TD rowspan="3"></TD>
+
+	<TD width="125"><A href="view/him.php" onMouseOver="swap('h');window.status='Химерики';
+	return true" onMouseOut="restore('h');window.status=''; return true">
+	<img src="img/bdahim.gif" alt="Химерики" border="0" name="h"></A></TD>
+
+<TD width="170"></TD>
+
+	<TD width="125"><A href="view/dit.php" onMouseOver="swap('d');window.status='Дитячі вірші';
+	return true" onMouseOut="restore('d');window.status=''; return true">
+	<img src="img/bdadit.gif" alt="Дитячі вірші" border="0" name="d"></A></TD>
+
+<TD rowspan="3"></TD>
+<TD rowspan="3" width="100" align="right">
+
+<script type='text/javascript' language='javascript'>
+r=escape(document.referrer); u=escape(document.URL);
+s=(typeof(screen)=='undefined')?'':screen.width+'x'+screen.height+'x'+(screen.colorDepth?screen.colorDepth:screen.pixelDepth);
+</script>
+<table cellpadding="0" cellspacing="0" border="0" width="88" height="31" style="line-height:0;width:88px;"><tr style="height:10px;">
+<td style="padding:0;width:38px;height:10px;"><a href="http://www.24log.de" target="_blank"><img src="http://counter.24log.ru/buttons/24/bg24-9_1.gif" width="38" height="10" border="0" alt="Besucherzahler" title="Besucherzahler " style="margin:0;padding:0;" /></a></td>
+<td style="padding:0;width:50px;height:10px;"><a href="http://www.1russianbrides.com" target="_blank"><img src="http://counter.24log.ru/buttons/24/bg24-9_3.gif" width="50" height="10" border="0" alt="hot and beautiful russian brides agency" style="margin:0;padding:0;"></a></td></tr>
+<tr style="height:21px;"><td style="padding:0;width:38px;height:21px"><a href="http://www.24log.ru" target="_blank"><img src="http://counter.24log.ru/buttons/24/bg24-9_2.gif" width="38" height="21" alt="счетчик посещений" title="счетчик посещений" border="0" style="margin:0;padding:0;" /></a></td>
+<script type='text/javascript' language='javascript'>
+document.write('<td style="padding:0px;width:50px;height:21px;"><a href="http://www.24log.ru/rating/rating.php?c=8" target="_blank"><img border="0" width="50" height="21" src="http://counter.24log.ru/counter?id=104614&t=24&st=9&r='+r+'&s='+s+'&u='+u+'&rnd='+Math.random()+'" alt="Рейтинг: Культура" title="Показано число просмотров всего и за сегодня" style="margin:0;padding:0;" /></a></td>');
+</script></tr></table>
+
+</TD>
+</TR>
+
+<TR>
+<TD></TD>
+
+	<TD><A href="view/old.php" onMouseOver="swap('o');window.status='Вірші 60х - 80х років';
+	return true" onMouseOut="restore('o');window.status=''; return true">
+	<img src="img/bdaold.gif" alt="Вірші 60х - 80х років" border="0" name="o"></A></TD>
+
+<TD></TD>
+</TR>
+
+<TR>
+
+
+
+<TD></TD>
+
+	<TD><A href="view/about.php" onMouseOver="swap('a');window.status='Про автора';
+	return true" onMouseOut="restore('a');window.status=''; return true">
+	<img src="img/bdaab.gif" alt="Про автора" border="0" name="a"></A></TD>
+	
+</TR>
+
+</TABLE>
+</CENTER>
+<BR><BR>
+
+<CENTER>
+<TABLE border="0" width="95%">
+
+<TR>
+<TD width="200" align="center"><FONT size="+1" FACE="Arial" color="#A60000">"Поет - вічний дисидент"</FONT><BR>
+<FONT FACE="Arial" color="#A60000"><I>Р. Струць</I></FONT></TD><TD align="right"></TD>
+<TD align="center"><FONT size="+1" FACE="Arial">
+*****<BR><BR>
+Мій предок пас козу на Пулківських висотах.<BR>
+Цареві звідти накрапав один відсоток.<BR>
+Засекспартнерили царевичі козу.<BR>
+Відтоді я не кожному скажу,<BR>
+що предок пас її на Пулківських висотах<BR><BR>
+*****
+<BR><BR>
+<FONT color="#FFFFFF">
+Доповнено 29.08.2012. Звізуалені вірші в розділі "Дитячі вірші"
+</FONT>
+</FONT>
+</TD>
+<TD align="left"></TD><TD width="200"><img src="img/portret.jpg" border="0" width="220" align="bottom"></TD>
+</TR>
+
+</TABLE>
+</CENTER><BR><BR>
+
+<CENTER>
+<TABLE border="0" width="95%">
+<TR>
+
+<TD width="50%" align="left">
+<a href=http://meta.ua><img src=http://meta.ua/img/banners/knopka1.gif width=88 height=31 border=0 alt="META - Украина. Украинская поисковая система"></a>
+
+</TD>
+<TD width="50%" align="right"></TD>
+
+</TR>
+</TABLE>
+</CENTER>
+
+<BR><BR>
+<CENTER>
+<HR width="200">
+<FONT size="-1">Зв'язатись з автором можна за адресою <A href="mailto:orklyn@yandex.ru">orklyn@yandex.ru</A></FONT>
+</CENTER>
+<BR>
+
+</body>
+</html>
---
+++ view/dit.php
@@ -0,0 +1,553 @@
+<HTML>
+
+<HEAD>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+
+
+<style type="text/css">
+<!--
+
+TABLE {	FONT-FAMILY: Arial }
+
+-->
+</style>
+
+<TITLE>Дитячі вірші</TITLE>
+</HEAD>
+
+<BODY bgcolor="black"  text="grea" link="#3399FF" vlink="magenta" alink="black">
+
+<CENTER><FONT face="Tahoma" color="red" size="+3"><B>Дитячі вірші</B></font></CENTER>
+<HR width="500" size="4"><BR>
+
+<SCRIPT LANGUAGE="JavaScript">
+<!--
+if (document.images)
+{
+var hoff = new Image();
+hoff.src = "img/bdahim.gif";
+var honn = new Image();
+honn.src = "img/bahim.gif";
+var ioff = new Image();
+ioff.src = "img/bdaind2.gif";
+var ionn = new Image();
+ionn.src = "img/baind2.gif";
+var ooff = new Image();
+ooff.src = "img/bdaold.gif";
+var oonn = new Image();
+oonn.src = "img/baold.gif";
+var boff = new Image();
+boff.src = "img/bdabook.gif";
+var bonn = new Image();
+bonn.src = "img/babook.gif";
+var aoff = new Image();
+aoff.src = "img/bdaab.gif";
+var aonn = new Image();
+aonn.src = "img/baab.gif";
+
+}
+function swap(imgName)
+{
+ if (document.images) document[imgName].src = eval(imgName + 'onn.src');
+}
+function restore(imgName)
+{
+if (document.images) document[imgName].src = eval(imgName + 'off.src');
+}
+// -->
+</SCRIPT>
+
+<CENTER>
+<TABLE border="0" width="90%">
+
+<TR>
+<TD rowspan="3" width="100"></TD>
+<TD rowspan="3"></TD>
+
+	<TD width="125"><A href="him.php" onMouseOver="swap('h');window.status='Химерики';
+	return true" onMouseOut="restore('h');window.status=''; return true">
+	<img src="bdahim.gif" alt="Химерики" border="0" name="h"></A></TD>
+
+<TD width="170"></TD>
+
+	<TD width="125"><A href="../index.php" onMouseOver="swap('i');window.status='Головна сторінка';
+	return true" onMouseOut="restore('i');window.status=''; return true">
+	<img src="bdaind2.gif" alt="Головна сторінка" border="0" name="i"></A></TD>
+
+<TD rowspan="3"></TD>
+<TD rowspan="3" width="100"></TD>
+</TR>
+
+<TR>
+<TD></TD>
+
+	<TD><A href="old.php" onMouseOver="swap('o');window.status='Вірші 60х - 80х років';
+	return true" onMouseOut="restore('o');window.status=''; return true">
+	<img src="bdaold.gif" alt="Вірші 60х - 80х років" border="0" name="o"></A></TD>
+
+<TD></TD>
+</TR>
+
+<TR>
+
+
+<TD></TD>
+
+	<TD><A href="about.php" onMouseOver="swap('a');window.status='Про автора';
+	return true" onMouseOut="restore('a');window.status=''; return true">
+	<img src="bdaab.gif" alt="Про автора" border="0" name="a"></A></TD>
+	
+</TR>
+
+</TABLE>
+</CENTER>
+
+<HR width="400" size="4"><BR>
+<CENTER>
+
+<FONT face="Tahoma" color="#A60000" size="+1"><B>ПІД ПСЕВДОМ </FONT><FONT face="Tahoma" color="#A60000" size="+2">ВІТЬ ВІТЬКО</B></FONT><BR><BR><BR>
+
+<TABLE border="0" align="center" width="90%">
+<TR>
+
+<TD align="center">
+
+<FONT face="Tahoma" color="#A60000" size="+1">З КНИЖЕЧКИ </FONT>
+<FONT face="Tahoma" color="#A60000" size="+2">"РАВЛИК-МУРАВЛИК" 1988 РІК</FONT><BR><BR>
+
+<FONT size="+1"><A href="ravlyk.pdf">Скачати книжку "Равлик-муравлик" (pdf)</A></FONT><BR><BR>
+
+<FONT color="red" size="+1">ГРАЄ КОНИК</FONT><BR><BR>
+Грає коник на одній струні - <BR>
+майже голосні його пісні.<BR>
+<BR>
+Джміль гуде, як десять літаків,<BR>
+та, мабуть, не всі джмелі такі.<BR>
+<BR>
+Жайворонка хтось на небо взяв<BR>
+і на довгу нитку прив'язав.<BR>
+<BR>
+І пошкутильгав за річку міст - <BR>
+вовк і лис тягли його за хвіст.<BR>
+<BR>
+А за ними я слідком ішов - <BR>
+і квітки цвіли з-під підошов.<BR><BR><BR>
+
+<FONT color="red" size="+1">КАЗОЧКА</FONT><BR><BR>
+В Цареграді жив-був лис<BR>
+не тепер і не колись.<BR>
+<BR>
+Сіло сонечко за ліс,<BR>
+лис на дерево заліз.<BR>
+<BR> 
+Жовтий місяць, а й лис<BR>
+на гілляці обнялись.<BR>
+<BR>
+"Лисе, вусом не колись!" -<BR>
+хтось казатиме колись.<BR><BR><BR>
+
+<FONT color="red" size="+1">ЛЕЛЕКА</FONT><BR><BR>
+Чи то клопіт,<BR>
+чи не клопіт? - <BR>
+загубив лелека чобіт.<BR>
+<BR>
+Загубив і не шукав,<BR>
+не шукав - <BR>
+босу ногу заховав<BR>
+у рукав.<BR>
+<BR>
+Ти б  ніколи ногу<BR>
+так не взув,<BR>
+а лелека ще й дратує<BR>
+козу:<BR>
+- Не стояти козі<BR>
+на одній нозі!<BR><BR><BR>
+
+<FONT color="red" size="+1">І НЕ МОРКВА</FONT><BR><BR>
+На вербі росте цибуля - <BR>
+не буряк,<BR>
+на майдані свиснув рак-неборак.<BR>
+<BR>
+Вулиця до неба стала сторч,<BR>
+іне так ходити треба - <BR>
+хоч-не-хоч!<BR>
+<BR>
+У кишені в мене жук, жук, жук,<BR>
+угадаєш у котрій - <BR>
+покажу!<BR>
+<BR>
+І не морква - не цибуля -<BR>
+не буряк -<BR>
+на майдані свиснув рак-неборак.<BR><BR><BR>
+
+<FONT color="red" size="+1">ТОПОЛЯТА</FONT><BR><BR>
+Попід муром тополята стоять -<BR>
+не чотири та й не п'ять тополят.<BR>
+<BR>
+Перший, щонайвищий, свище:<BR>
+я найви,<BR>
+я найви,<BR>
+я найвищий!<BR>
+<BR>
+Другий - не такий, як усі, -<BR>
+на бруківку жовте листя<BR>
+обтрусив.<BR>
+<BR>
+А найменший тополин<BR>
+хвацько шапку заломив:<BR>
+<BR>
+- Ну ж бо, хлопці,<BR>
+кроком руш -<BR>
+на вечерю хочу груш!<BR><BR><BR>
+
+<FONT color="red" size="+1">ЛЕВИ</FONT><BR><BR>
+А мої дерева -<BR>
+схожі на лева.<BR>
+<BR>
+Разом лапи склали -<BR>
+в гриви заховали.<BR>
+<BR>
+А хто каже: леви,<BR>
+леви кришталеві.<BR>
+<BR>
+Разом лапи склали -<BR>
+в гриви заховали.<BR>
+<BR>
+Ой випало снігу -<BR>
+левам не до сміху.<BR>
+<BR>
+Разом лапи склали -<BR>
+в гриви заховали.<BR><BR><BR>
+
+<FONT color="red" size="+1">МАЛИНОВІ КОНІ</FONT><BR><BR>
+Як захочу - маю<BR>
+малинові коні,<BR>
+<BR>
+малинові коні -<BR>
+на долоні,<BR>
+<BR>
+на моїй долоні -<BR>
+ягоди червоні.<BR><BR><BR>
+
+<FONT face="Tahoma" color="#A60000" size="+1">З КНИЖЕЧКИ </FONT>
+<FONT face="Tahoma" color="#A60000" size="+2">"ГОЙДА - РАЗ, ГОЙДА - ДВА" 1989 РІК</FONT><BR><BR>
+
+<FONT size="+1"><A href="hojda.pdf">Скачати книжку "Гойда-раз, гойда-два" (pdf)</A></FONT><BR><BR>
+
+<FONT color="red" size="+1">ГОЙДАЛКА</FONT><BR><BR>
+Гойда - раз, гойда - два,<BR>
+гойдаються дерева.<BR>
+<BR>
+Деревa-дерeва - <BR>
+наша королева.<BR>
+<BR>
+Наша королева<BR>
+сіла на лева.<BR>
+<BR>
+Гойда - раз, гойда - два,<BR>
+гойдаються дерева.<BR><BR><BR>
+
+<FONT color="red" size="+1">ПІВНИК</FONT><BR><BR>
+Тигри ходять по квартирі:<BR>
+два та два - чотири тигри.<BR>
+<BR>
+І висока жовта шафа<BR>
+обернулась на жирафа.<BR>
+<BR>
+Щоб її не з'їли тигри:<BR>
+раз-два-три-чотири!<BR>
+<BR>
+А малий червоний півник<BR>
+заховався у годинник.<BR>
+<BR>
+І дзьобає синій мак<BR>
+тільки так:<BR>
+тік-так, тік-так...<BR><BR><BR>
+
+<FONT color="red" size="+1">ПІР'ЇНКА</FONT><BR><BR>
+Червонобоке яблуко,<BR>
+перекинся в зяблика.<BR>
+<BR>
+Зябку-зяблику малий,<BR>
+левенятко примани.<BR>
+<BR>
+За горою, за отою -<BR>
+пір'їнкою золотою.<BR><BR><BR>
+
+<FONT color="red" size="+1">КАШТАН</FONT><BR><BR>
+Аж там - <BR>
+на горі каштан.<BR>
+<BR>
+Засвітив свічки<BR>
+біля кринички.<BR>
+<BR>
+Піду по  воду,<BR>
+піду по холодну.<BR>
+<BR>
+Навшпиньки стану -<BR>
+свічечку дістану.<BR><BR><BR>
+
+<FONT color="red" size="+1">ПОМАРАНЧ</FONT><BR><BR>
+Два коти, три коти<BR>
+з глини-теракоти.<BR>
+<BR>
+Пішли на Підзамча,<BR>
+знайшли помаранча.<BR>
+<BR>
+Кіт котові каже: "Ач!<BR>
+справді гарний помаранч.<BR>
+<BR>
+Даймо його мавпі<BR>
+поділити навпіл".<BR><BR><BR>
+
+<FONT color="red" size="+1">ДУНАЙ</FONT><BR><BR>
+Біля моря, дле Дунаю<BR>
+з вуха воду виливаю.<BR>
+<BR>
+Чотирнадцять відер<BR>
+затопили Відень.<BR>
+<BR>
+Тра' човника рихтувати,<BR>
+щоб віденців рятувати.<BR><BR><BR>
+
+<FONT face="Tahoma" color="#A60000" size="+1">З НЕВИДРУКОВАНОЇ КНИЖЕЧКИ </FONT>
+<FONT face="Tahoma" color="#A60000" size="+2">"ЇВ ТРОЛЕЙБУС БУЛКУ З МАКОМ"</FONT><BR><BR>
+
+<FONT size="+1"><A href="trolejbus.pdf">Скачати макет книжки "Їв тролейбус булку з маком" (pdf)</A></FONT><BR><BR>
+
+<FONT color="red" size="+1">ВЕЧЕРЯ</FONT><BR><BR>
+На блакитному папері<BR>
+намалюю сині двері.<BR>
+<BR>
+Сині вікна й синій ліс,<BR>
+ще й будинок без коліс.<BR>
+<BR>
+Під будинком є печера,<BR>
+у печері є вечеря:<BR>
+<BR>
+Три тарілки, ложка, пляшка,<BR>
+а на пляшці - синя пташка.<BR><BR><BR>
+
+<FONT color="red" size="+1">ЗВІР ТАКИЙ</FONT><BR><BR>
+Як по ниточці ходив<BR>
+звір такий собі один.<BR>
+<BR>
+В нього роги є аж два<BR>
+й отакенна голова!<BR>
+<BR>
+Голова, немов казан,<BR>
+щоб нікому не казав,<BR>
+<BR>
+ніби весело жукові<BR>
+у коробці сірниковій.<BR><BR><BR>
+
+<FONT color="red" size="+1">ДВІ КОСИ</FONT><BR><BR>
+Я веду козу<BR>
+через дерезу.<BR>
+<BR>
+Рогату козу<BR>
+заплету в косу.<BR>
+<BR>
+А на дві коси<BR>
+треба дві кози.<BR><BR><BR>
+
+<FONT color="red" size="+1">ВОРОН</FONT><BR><BR>
+На городі дички -<BR>
+ондечки.<BR>
+<BR>
+Що на першій дичці,<BR>
+я не бачу звідси.<BR>
+<BR>
+А на другій - он!..<BR>
+кряче ворон.<BR>
+<BR>
+І чого він кряче,<BR>
+і чого неначе<BR>
+<BR>
+очі-намистини<BR>
+на хліб намастили?<BR><BR><BR>
+
+<FONT color="red" size="+1">СТОНОГА ПЕРЕ ШКАРПЕТКИ</FONT><BR><BR>
+І казати навіть сором,<BR>
+що під нашим осокором<BR>
+<BR>
+30 - 40 - 50<BR>
+на мотузочці висять.<BR>
+<BR>
+Як висять і чом висять,<BR>
+і чому їх 50,<BR>
+<BR>
+і чого казати сором,<BR>
+що насправді тільки 40?<BR><BR><BR>
+
+<FONT color="red" size="+1">ТРОЛЕЙБУС</FONT><BR><BR>
+Загадаю тобі ребус<BR>
+про дванадцятий тролейбус.<BR>
+<BR>
+Кругом нього хлопці босі<BR>
+тупцювали на морозі.<BR>
+<BR>
+Їв тролейбус булку з маком<BR>
+і чогось тихенько плакав.<BR><BR><BR>
+
+<FONT face="Tahoma" color="#A60000" size="+1">З ЕВЕНТУАЛЬНОЇ КНИЖЕЧКИ </FONT>
+<FONT face="Tahoma" color="#A60000" size="+2">"ЕРБОД ІЦИК"</FONT><BR><BR>
+
+<FONT color="red" size="+1">ЯЦИК</FONT><BR><BR>
+Аліс Яцик ан аток.<BR>
+Нащо киці молоток?<BR>
+<BR>
+Молоток - котолом.<BR>
+Ербод Іцик із котом.<BR><BR><BR>
+
+<FONT color="red" size="+1">
+ЗІРКА</FONT><BR><BR>
+Дідусеві окуляри<BR>
+хтозна-де гуляли.<BR>
+<BR>
+Прийшли були о четвертій<BR>
+ні живі ні мертві.<BR>
+<BR>
+Праве шкельце стало синє,<BR>
+як небо осіннє.<BR>
+<BR>
+Ну а ліве - щойно вранці<BR>
+ледь постало в рамці.<BR>
+<BR>
+Від роси черешня згіркла,<BR>
+а сонечко зайде -<BR>
+<BR>
+посміхнеться в небі зірка<BR>
+і оближе заїди.<BR><BR><BR>
+
+<FONT color="red" size="+1">ПАННОЧКА</FONT><BR><BR>
+Панночка Галя<BR>
+на скрипочку грала.<BR>
+<BR>
+Що на першій струні -<BR>
+квіти розцвіли.<BR>
+<BR>
+Що була зросла<BR>
+край поля сосна.<BR>
+<BR>
+Що хлопці-молодці<BR>
+не ходять по струнці.<BR> 
+<BR>
+На те вони хлопці.<BR>
+І всі вони звідти,<BR>
+<BR>
+звідки грає чайка<BR>
+на синьому вітрі.<BR><BR><BR>
+
+<FONT color="red" size="+1">
+КАЖАН</FONT><BR><BR>
+Ми впіймали кажана.<BR>
+Кажаниха каже: на!<BR>
+Нате по півгривні,<BR>
+<BR>
+аби папі в Римі<BR>
+не боліла голова<BR>
+ще й за мого янгола.<BR>
+<BR>
+Будьте-но привітні -<BR>
+відпустіте кажана<BR>
+під чотири вітрі.<BR><BR><BR>
+
+<FONT color="red" size="+1">МАШИНА</FONT><BR><BR>
+Тільки в нашої машини -<BR>
+очі мурашині.<BR>
+<BR>
+То дарма,<BR>
+що машина без керма.<BR>
+<BR>
+Без керма, ще й без коліс -<BR>
+мають вирости колись.<BR><BR><BR>
+
+<FONT color="red" size="+1">ДВІ СТЕЖКИ</FONT><BR><BR>
+На ліщині дві сережки.<BR>
+Під ліщиною дві стежки.<BR>
+<BR>
+Перша стежка - в чисте поле,<BR>
+де їжак гуляє голий.<BR>
+<BR>
+Загубив їжак піджак,<BR>
+і не колеться їжак.<BR>
+<BR>
+Друга стежка навпаки -<BR> 
+там гуляють літаки.<BR>
+<BR>
+І під музику іржаву<BR>
+жук везе Царівну Жабу.<BR><BR><BR>
+
+
+<FONT color="red" size="+1">
+РІЗДВО</FONT><BR><BR>
+До середини зими<BR>
+вісім кроків без семи.<BR>
+<BR>
+Раз-два -<BR> 
+дехто лічить від Різдва.<BR>
+<BR>
+Три-чотири -<BR>
+вовка в хаті причинили.<BR>
+<BR>
+П’ять-шість -<BR>
+прийде зайчик - вовка з’їсть.<BR>
+<BR>
+Сім -<BR> 
+стане добре, та не всім.<BR>
+<BR>
+Залишімо вовка жити -<BR>
+хай жує шкоринки житні.<BR><BR><BR>
+
+<FONT color="red" size="+1">
+САМ У ХАТІ</FONT><BR><BR>
+Песик-персик грався нишком<BR>
+(чоботи хропли під ліжком!)<BR>
+<BR>
+Над столом рябіла мапа:<BR>
+там, де Африка бахмата,<BR>
+<BR>
+лев розлігся на городі<BR>
+із цигаркою у роті.<BR>
+<BR>
+Песик-персик кашу виїв<BR>
+і сльозу струсив із вії.<BR><BR><BR>
+
+<FONT color="red" size="+1">РАВКА</FONT><BR><BR>
+Равлик чорний - равка біла.<BR>
+Білій равці каша збігла.<BR>
+<BR>
+Равка вдруге кашу варить.<BR>
+Друга каша не завадить.<BR>
+<BR>
+Другу кашу равлик з’їв.<BR>
+Повних ложок сорок сім.<BR><BR><BR> 
+
+<FONT face="Tahoma" color="#A60000" size="+1">ЗВІЗУАЛЕНІ ВІРШІ </FONT>
+<FONT face="Tahoma" color="#A60000" size="+2">"ПОЕЗОМАЛЯРСТВО"</FONT><BR><BR>
+
+<img src="rotor2.JPG" ><BR><BR><BR>
+
+<img src="yabluko.JPG" ><BR><BR><BR>
+
+<img src="morozyvo.JPG" ><BR><BR><BR>
+
+ 
+
+</TD>
+
+</TR>
+</TABLE>
+</CENTER>
+
+<BR><BR>
+<CENTER>
+<HR width="200">
+<FONT size="-1">Зв'язатись з автором можна за адресою <A href="mailto:orklyn@yandex.ru">orklyn@yandex.ru</A></FONT>
+</CENTER>
+<BR>
+
+
+</body>
+</html>
